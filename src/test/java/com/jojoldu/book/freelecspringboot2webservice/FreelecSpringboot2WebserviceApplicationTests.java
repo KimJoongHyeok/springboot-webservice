f559336f7f5d2065730b20fc23b368fa1cc2c5c6
KimJoongHyeok <moon960427@gmail.com>
2022-02-12 13:50:43 +0900

    first commit

---
+++ src/test/java/com/jojoldu/book/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.jojoldu.book.freelecspringboot2webservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringboot2WebserviceApplicationTests {
+
+
+}
