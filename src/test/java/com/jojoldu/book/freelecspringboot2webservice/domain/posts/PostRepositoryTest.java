31ab46a75562a405ef2e408f49baeb334a09c375
KimJoongHyeok <moon960427@gmail.com>
2022-02-16 11:42:01 +0900

    3.5 JPA  Auditing으로 생성시간/수정시간 자동화하기

--- src/test/java/com/jojoldu/book/freelecspringboot2webservice/domain/posts/PostRepositoryTest.java
+++ src/test/java/com/jojoldu/book/freelecspringboot2webservice/domain/posts/PostRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,12 +20,12 @@ public class PostRepositoryTest {
     PostsRepository postsRepository;
 
     @After //Junit에서 단위테스트가 끝날 때마다 수행되는 메소드를 지정 , 전체 테스트를 수행할 때 테스트간 데이터 침범을 막기 위해 사요함
-    public void cleanup(){
-        postsRepository.deleteAll();;
+    public void cleanup() {
+        postsRepository.deleteAll();
     }
 
     @Test
-    public void 게시글저장_불러오기(){
+    public void 게시글저장_불러오기() {
         //given
         String title = "테스트게시글";
         String content = "테스트본문";
@@ -43,5 +44,24 @@ public class PostRepositoryTest {
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
 
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>> createDate = " + posts.getCreatedDate() + ", moifiedDate = " + posts.getModifiedDate());
     }
 }
