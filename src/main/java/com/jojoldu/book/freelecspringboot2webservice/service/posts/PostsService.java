22c6dcb25f83e0b5664f9f4ccb8009c9f5b1c78d
KimJoongHyeok <moon960427@gmail.com>
2022-04-03 23:36:29 +0900

    4.5 게시글 수정,삭제 화면 만들기

--- src/main/java/com/jojoldu/book/freelecspringboot2webservice/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/freelecspringboot2webservice/service/posts/PostsService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -47,5 +48,13 @@ public class PostsService {
         return postsRepository.findAllDesc().stream()
                 .map(PostsListResponseDto::new)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        postsRepository.delete(posts);
     }
 }
