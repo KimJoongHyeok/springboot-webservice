52b88ed9462dce3a12df8d47150f013fa9d4dd74
KimJoongHyeok <moon960427@gmail.com>
2022-04-04 17:16:30 +0900

    5.3 구글 로그인 연동하기

--- src/main/java/com/jojoldu/book/freelecspringboot2webservice/web/IndexController.java
+++ src/main/java/com/jojoldu/book/freelecspringboot2webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.freelecspringboot2webservice.web;
 
+import com.jojoldu.book.freelecspringboot2webservice.config.auth.dto.SessionUser;
 import com.jojoldu.book.freelecspringboot2webservice.service.posts.PostsService;
 import com.jojoldu.book.freelecspringboot2webservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
+
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("LoginUserName", user.getName());
+        }
+
         return "index";
     }
 
