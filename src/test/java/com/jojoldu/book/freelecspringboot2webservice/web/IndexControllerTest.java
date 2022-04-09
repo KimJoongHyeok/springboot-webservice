9e510eaa497ec27d787e0c1181da5da646c69b2d
KimJoongHyeok <moon960427@gmail.com>
2022-04-09 20:28:59 +0900

    5.7 기존 테스트에 시큐리티 적용하기

--- src/test/java/com/jojoldu/book/freelecspringboot2webservice/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/book/freelecspringboot2webservice/web/IndexControllerTest.java
@@ -15,14 +15,14 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 public class IndexControllerTest {
 
     @Autowired
-    private TestRestTemplate testRestTemplate;
+    private TestRestTemplate restTemplate;
 
     @Test
-    public void 메인페이지_로딩(){
+    public void 메인페이지_로딩() {
         //when
-        String body = this.testRestTemplate.getForObject("/",String.class);
+        String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
 }
